d759d4278af2fa47af01383933fc4e034ad16587
YB Kim <ybkim@YB-Kimui-MacBookAir.local>
2022-01-14 15:46:48 +0900

    Initial commit

---
+++ src/Test01.java
@@ -0,0 +1,13 @@
+public class Test01 {
+
+    public static void main(String[] args) {
+
+        for (int i=1; i <= 5; i++) {
+            for (int j=1; j <= 5; j++) {
+                if (i <= j) {
+                    System.out.println("i : " + i + ",j : " + j);
+                }
+            }
+        }
+    }
+}
