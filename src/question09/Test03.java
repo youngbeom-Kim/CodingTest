4c763736535a2cf167ed07a595b17719d7b6d7cc
ybkim <abcd450196@gmail.com>
2022-01-25 03:43:37 +0900

    Question09 - Test03, 04 Question10 - Test01

---
+++ src/question09/Test03.java
@@ -0,0 +1,17 @@
+package question09;
+
+public class Test03 {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+            if (i % 10 == 0) {
+                System.out.printf("1 ~ %3d : %d\n", i, sum);
+            }
+        }
+
+    }
+}
