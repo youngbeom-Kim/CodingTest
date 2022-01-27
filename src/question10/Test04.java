f89382e35ea99d6750fa70bcf3331bc1009f2ee5
ybkim <abcd450196@gmail.com>
2022-01-28 04:22:34 +0900

    Question10 - Test04

---
+++ src/question10/Test04.java
@@ -0,0 +1,13 @@
+package question10;
+
+//파일명 10개를 입력받아 각 확장자별로 총 개수를 출력하시오,
+//확장자는 다음으로 제한한다. (gif, jpg, png, hwp, doc)
+public class Test04 {
+
+    public static void main(String[] args) {
+
+        int gif = 0;
+        int jpg = 0;
+
+    }
+}
