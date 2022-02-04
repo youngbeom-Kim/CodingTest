074eef561ec4d7483aba21622cbf450b173d551d
ybkim <abcd450196@gmail.com>
2022-02-05 01:40:31 +0900

    Question11 - Test01,02

--- src/question11/Test01.java
+++ src/question11/Test01.java
@@ -5,7 +5,31 @@ public class Test01 {
 
     public static void main(String[] args) {
 
+        int[] list = new int[4];
+        list[0] = 10;
+        list[1] = 20;
+        list[2] = 30;
+        list[3] = 40;
+        String result = dump(list);
 
+        System.out.printf("nums = %s\n", result);
+
+    }
+
+    private static String dump(int[] list) {
+
+        String str = "[ ";
+
+        for (int i = 0; i < list.length; i++) {
+            str += list[i];
+            if (i == list.length - 1) {
+                break;
+            }
+            str += ",";
+        }
+        str += " ]";
+
+        return str;
 
     }
 }
