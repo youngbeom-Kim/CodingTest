7b4261b448bba0a5ad6f0525c2d89f8a66a71ec9
ybkim <abcd450196@gmail.com>
2022-02-14 00:59:54 +0900

    Question14 - Test02 Question15 - Test01

--- src/question14/Test02.java
+++ src/question14/Test02.java
@@ -21,4 +21,154 @@ package question14;
 // Note가 잘 설계되었는지 객체를 가지고 사용해본다.
 // - Note 객체 생성 -> 값 넣기 + 값 출력 등
 public class Test02 {
+
+    public static void main(String[] args) {
+
+        Note note = new Note();
+        note.setSize("B4");
+        note.setColor("노란색");
+        note.setPage(25);
+        note.setOwner("홍길동");
+        System.out.println(note.info());
+
+        Note note2 = new Note();
+        note2.setSize("A4");
+        note2.setColor("검정색");
+        note.setPage(100);
+        System.out.println(note2.info());
+
+
+    }
 }
+
+class Note {
+
+    private String size = "A5";
+    private String color = "흰색";
+    private int page = 10;
+    private String owner;
+    private int price;
+
+    //getter, setter 구현
+
+    public void setSize(String size) {
+        if (size.equals("A3") || size.equals("A4") || size.equals("A5")
+                || size.equals("B3") || size.equals("B4") || size.equals("B5")) {
+            this.size = size;
+        } else {
+            return;
+        }
+    }
+
+    public void setColor(String color) {
+        if (color.equals("흰색") || color.equals("검정색")
+                || color.equals("노란색") || color.equals("파란색")) {
+            this.color = color;
+        } else {
+            return;
+        }
+    }
+
+    public void setPage(int page) {
+        if (page >= 10 && page <= 200) {
+            this.page = page;
+        } else {
+            this.page = page;
+        }
+    }
+
+    public void setOwner(String owner) {
+        for (int i = 0; i < owner.length(); i++) {
+            if (owner.length() >= 2 && owner.length() <= 5) {
+                char check = owner.charAt(i);
+                if (check >= '가' && check <= '힣') {
+                    this.owner = owner;
+                } else {
+                    return;
+                }
+            } else {
+                this.owner = "이름은 한글 2 ~ 5자 이내만 가능합니다.";
+            }
+        }
+    }
+
+    public String info() {
+
+        int price = 500;
+        if (size.equals("A3")) {
+            price += 400;
+        } else if (size.equals("A4")) {
+            price += 200;
+        } else if (size.equals("B3")) {
+            price += 500;
+        } else if (size.equals("B4")) {
+            price += 300;
+        } else if (size.equals("B5")) {
+            price += 100;
+        }
+
+        if (color.equals("검정색")) {
+            price += 100;
+        } else if (color.equals("노란색")) {
+            price += 200;
+        } else if (color.equals("파란색")) {
+            price += 200;
+        }
+
+        int count = 0;
+        for (int i = 10; i < page; i++) {
+            count++;
+        }
+        price += count * 10;
+
+        String thickness = "";
+        if (page >= 10 && page <= 50) {
+            thickness += "얇은";
+        } else if (page >= 51 && page <= 100) {
+            thickness += "보통";
+        } else if (page >= 101 && page <= 200) {
+            thickness += "두꺼운";
+        } else {
+            thickness += "노트는 10 ~ 200장 사이만 가능합니다.";
+            return thickness;
+        }
+
+        String str = "";
+        str += "■■■■■■ 노트 정보 ■■■■■■\n";
+        str += String.format("소유자 : %s\n", owner);
+        str += String.format("특성 : %s %s %s노트\n", color, thickness, size);
+        str += String.format("가격 : %,d원\n", price);
+        str += "■■■■■■■■■■■■■■■■■■■■■\n";
+
+        if (str.indexOf("null") > 0) {
+            str = "";
+            str += "■■■■■■ 노트 정보 ■■■■■■\n";
+            str += "주인 없는 노트\n";
+            str += "■■■■■■■■■■■■■■■■■■■■■\n";
+        }
+
+        return str;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
