dc567d41cb9e98a4dfab6b7c793dc5908c0f1346
ybkim <abcd450196@gmail.com>
2022-02-17 20:02:41 +0900

    Question19 - Main, MyHashMap

---
+++ src/question19/MyHashMap.java
@@ -0,0 +1,12 @@
+package question19;
+
+//MyHashMap
+//추가, 수정 = void put(String key, String value)
+//읽기 = String get(String key)
+//크기 = int size()
+//삭제 = void remove(String key)
+//검색(key) = boolean containKey(String key)
+//검색(value) = boolean containValue(String value)
+//초기화 = void clear()
+public class MyHashMap {
+}
