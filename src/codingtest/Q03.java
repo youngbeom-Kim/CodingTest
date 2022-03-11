64df59aefab2f1869b3c71d30f394038662e7d87
ybkim <abcd450196@gmail.com>
2022-03-11 23:53:08 +0900

    Q03 - 모의고사

---
+++ src/codingtest/Q03.java
@@ -0,0 +1,16 @@
+package codingtest;
+
+//프로그래머스 > 모의고사
+public class Q03 {
+    public int[] solution(int[] answers) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Q03 sol = new Q03();
+        System.out.println();
+
+    }
+}
