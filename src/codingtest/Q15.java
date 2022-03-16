d6a28fc4816a11aeb5bbde538d51a5d8a6f5a594
ybkim <abcd450196@gmail.com>
2022-03-17 04:40:19 +0900

    Q13 - 3진법 뒤집기 Q14 - 약수의 개수와 덧셈 Q15 - 내적

---
+++ src/codingtest/Q15.java
@@ -0,0 +1,23 @@
+package codingtest;
+
+//프로그래머스 > 내적
+public class Q15 {
+    public int solution(int[] a, int[] b) {
+        int answer = 1234567890;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Q15 sol = new Q15();
+
+        int[] a = {1, 2, 3, 4};
+        int[] b = {-3, -1, 0, 2};
+        System.out.println(sol.solution(a, b));
+
+        int[] a2 = {-1, 0, 1};
+        int[] b2 = {1, 0, -1};
+        System.out.println(sol.solution(a, b));
+
+    }
+}
