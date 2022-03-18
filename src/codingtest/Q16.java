d9c6761cb9f85bda7d9eaf1cf4dd17882dd64210
ybkim <abcd450196@gmail.com>
2022-03-19 06:47:08 +0900

    Q16 - 실패율

--- src/codingtest/Q16.java
+++ src/codingtest/Q16.java
@@ -1,9 +1,26 @@
 package codingtest;
 
+import java.util.HashMap;
+
 //프로그래머스 > 실패율
 public class Q16 {
     public int[] solution(int N, int[] stages) {
         int[] answer = {};
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int failed = 0;
+
+        for (int i = 0; i < N; i++) {
+            int count = 0;
+            for (int j = 0; j < stages.length; j++) {
+                if (i + 1 == stages[j]) {
+                    count++;
+                }
+            }
+            failed = count/stages.length;
+        }
+
         return answer;
     }
 
