d8b8fef7615217b86dc4dab94ec0dc80990f10b1
ybkim <abcd450196@gmail.com>
2022-04-01 18:12:31 +0900

    Q17 - 신규 아이디 추천

--- src/codingtest/Q17.java
+++ src/codingtest/Q17.java
@@ -1,9 +1,20 @@
 package codingtest;
 
+import java.util.Locale;
+
 //프로그래머스 > 신규 아이디 추천
 public class Q17 {
     public String solution(String new_id) {
         String answer = "";
+
+        //1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
+        String word = new_id.toLowerCase();
+
+        //2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
+        for (int i = 0; i < word.length(); i++) {
+
+        }
+
         return answer;
     }
 
