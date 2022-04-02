295d1873d9285f9c945b39c4f36d698cd7b6864f
ybkim <abcd450196@gmail.com>
2022-04-02 23:23:50 +0900

    Q17 - 신규 아이디 추천

--- src/codingtest/Q17.java
+++ src/codingtest/Q17.java
@@ -8,12 +8,20 @@ public class Q17 {
         String answer = "";
 
         //1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
-        String word = new_id.toLowerCase();
+        String word1 = new_id.toLowerCase();
 
         //2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
-        for (int i = 0; i < word.length(); i++) {
+        String word2 = "";
 
+        for (int i = 0; i < word1.length(); i++) {
+            char c = word1.charAt(i);
+            if (c >= 97 && c <= 122 || c >= 48 && c <= 57 || c == '-' || c == '_' || c == '.') {
+                word2 += c;
+            }
         }
+
+        //3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
+
 
         return answer;
     }
