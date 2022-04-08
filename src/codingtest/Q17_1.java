31b81087f538574de21e54590551b635eb55bc85
ybkim <abcd450196@gmail.com>
2022-04-08 19:10:34 +0900

    Q17_1 - 신규 아이디 추천 Q19 - 숫자 문자열과 영단어

--- src/codingtest/Q17_1.java
+++ src/codingtest/Q17_1.java
@@ -6,15 +6,35 @@ import java.util.Locale;
 public class Q17_1 {
     public String solution(String new_id) {
 
-        String answer = "";
-
         //1단계
         String temp = new_id.toLowerCase();
 
         //2단계
         temp = temp.replaceAll("[^-_.a-z0-9]", "");
 
-        return answer;
+        //3단계
+        temp = temp.replaceAll("[.]{2,}", ".");
+
+        //4단계
+        temp = temp.replaceAll("^[.]|[.]$", "");
+
+        //5단계
+        if (temp.equals("")) temp += "a";
+
+        //6단계
+        if (temp.length() >= 16) {
+            temp = temp.substring(0, 15);
+            temp = temp.replaceAll("^[.]|[.]$", "");
+        }
+
+        //7단계
+        if (temp.length() <= 2) {
+            while (temp.length() < 3) {
+                temp += temp.charAt(temp.length() - 1);
+            }
+        }
+
+        return temp;
     }
 
     public static void main(String[] args) {
