9f7a888a5f0bac040abd3bcca795334eb9ece8af
ybkim <abcd450196@gmail.com>
2022-04-11 17:31:11 +0900

    Q20 - 키패드 누르기

--- src/codingtest/Q20.java
+++ src/codingtest/Q20.java
@@ -5,7 +5,52 @@ public class Q20 {
     public String solution(int[] numbers, String hand) {
         String answer = "";
 
+        //기본 손의 위치는 왼손은 *, 오른손은 #
+        int leftN = 10;
+        int rightN = 12;
 
+        //누르는 번호 불러오기
+        for (int n : numbers) {
+            //왼손으로 누를 번호와 그 누른 번호의 위치 저장하기
+            if (n == 1 || n == 4 || n == 7) {
+                answer += "L";
+                leftN = n;
+            //오른손으로 누를 번호와 그 누른 번호의 위치 저장하기
+            } else if (n == 3 || n == 6 || n == 9) {
+                answer += "R";
+                rightN = n;
+            //가운데에 있는 번호의 누를 조건 구하기
+            } else {
+
+                //만약 눌러야하는 숫자가 0이면 11을 더하기
+                if (n == 0) {
+                    n += 11;
+                }
+
+                //누르는 번호와 손가락의 위치 거리 구하기
+                int leftD = (Math.abs(n - leftN)) / 3 + (Math.abs(n - leftN)) % 3;
+                int rightD = (Math.abs(n - rightN)) / 3 + (Math.abs(n - rightN)) % 3;
+
+                //거리가 같다면 주로 사용하는 손을 이용하기
+                if (leftD == rightD) {
+                    if (hand.equals("right")) {
+                        answer += "R";
+                        rightN = n;
+                    } else {
+                        answer += "L";
+                        leftN = n;
+                    }
+                //거리가 왼손보다 오른손이 가까우면 R
+                } else if (leftD > rightD) {
+                    answer += "R";
+                    rightN = n;
+                //거리가 오른손보다 왼손이 가까우면 L
+                } else {
+                    answer += "L";
+                    leftN = n;
+                }
+            }
+        }
 
         return answer;
     }
