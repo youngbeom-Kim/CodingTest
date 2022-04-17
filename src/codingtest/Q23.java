5155c7a66570a78109f4edf7ebc67a799e95a058
ybkim <abcd450196@gmail.com>
2022-04-17 23:52:24 +0900

    Q23 - [1차]다트 게임

--- src/codingtest/Q23.java
+++ src/codingtest/Q23.java
@@ -6,11 +6,37 @@ public class Q23 {
         int answer = 0;
 
         char[] charArr = dartResult.toCharArray();
+        int[] score = new int[3];
+        int count = -1;
 
         for (int i = 0; i < charArr.length; i++) {
-
+            if (charArr[i] >= '0' && charArr[i] <= '9') {
+                count++;
+                if (charArr[i] == '1' && charArr[i + 1] == '0') {
+                    score[count] = 10;
+                    i++;
+                } else {
+                    score[count] = charArr[i] - '0';
+                }
+            } else if (charArr[i] == 'D') {
+                score[count] *= score[count];
+            } else if (charArr[i] == 'T') {
+                score[count] *= score[count] * score[count];
+            } else if (charArr[i] == '*') {
+                if (count > 0) {
+                    score[count - 1] *= 2;
+                }
+                score[count] *= 2;
+            } else if (charArr[i] == '#') {
+                score[count] *= -1;
+            }
         }
 
+        for (int s : score) {
+            System.out.print(s + " ");
+        }
+
+        answer = score[0] + score[1] + score[2];
 
         return answer;
     }
@@ -19,7 +45,7 @@ public class Q23 {
 
         Q23 sol = new Q23();
 
-        String dartResult = "1S2D*#T";
+        String dartResult = "1S2D*3T";
         System.out.println(sol.solution(dartResult));
 
         String dartResult2 = "1D2S#10S";
