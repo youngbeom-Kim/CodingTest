5447b3d27fbed19ea0d7c676de0337a994bc1366
youngbeom-Kim <abcd450196@gmail.com>
2022-09-27 22:59:05 +0900

    최솟값 만들기

---
+++ src/level02/Q08.java
@@ -0,0 +1,34 @@
+package level02;
+
+import java.util.Arrays;
+
+//프로그래머스 > level02 > 최솟값 만들기
+public class Q08 {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for (int i = 0; i < A.length; i++) {
+            int a = A[i];
+            int b = B[B.length - i - 1];
+            answer += a * b;
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+
+        Q08 sol = new Q08();
+
+        int[] A = {1, 4, 2};
+        int[] B = {5, 4, 4};
+        System.out.println(sol.solution(A, B));
+
+        int[] A2 = {1, 2};
+        int[] B2 = {3, 4};
+        System.out.println(sol.solution(A2, B2));
+
+    }
+}
