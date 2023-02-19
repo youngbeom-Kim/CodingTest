0cf75f7d38b3d814a07592b1f48230a8998f79ad
youngbeom-Kim <abcd450196@gmail.com>
2023-02-19 13:16:18 +0900

    Level 01 > 정수 내림차순으로 배치하기, 나머지가 1이 되는 수 찾기, 콜라츠 추측

---
+++ src/level01/Q37.java
@@ -0,0 +1,31 @@
+package level01;
+
+//프로그래머스 > Level 01 > 콜라츠 추측
+public class Q37 {
+    public int solution(long num) {
+        int answer = 0;
+
+        while (num > 1) {
+            if (num % 2 == 0) {
+                answer++;
+                num = num / 2;
+            } else if (num % 2 == 1) {
+                answer++;
+                num = (num * 3) + 1;
+            }
+        }
+
+        if (answer >= 500) {
+            answer = -1;
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+
+        System.out.println(new Q37().solution(6));
+        System.out.println(new Q37().solution(16));
+        System.out.println(new Q37().solution(626331));
+
+    }
+}
