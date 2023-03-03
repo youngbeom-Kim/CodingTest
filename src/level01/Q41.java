ab56ff644a82b3334fc9ee4fe9fc50289cbdc745
youngbeom-Kim <abcd450196@gmail.com>
2023-03-03 18:48:42 +0900

    Level 01 > 문자열 다루기 기본, 부족한 금액 계산기, 행렬의 덧셈

---
+++ src/level01/Q41.java
@@ -0,0 +1,35 @@
+package level01;
+
+//프로그래머스 > Level01 > 문자열 다루기 기본
+public class Q41 {
+    public boolean solution(String s) {
+        boolean answer = true;
+
+        char[] cArr = s.toCharArray();
+
+        if (cArr.length > 6 || cArr.length < 4 || cArr.length == 5) {
+            return answer = false;
+        }
+
+        for (int i = 0; i < cArr.length; i++) {
+            if (cArr[i] < 48 || cArr[i] > 57) {
+                answer = false;
+            } else {
+                continue;
+            }
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+
+        Q41 sol = new Q41();
+
+        String s = "a234";
+        System.out.println(sol.solution(s));
+
+        String s2 = "1234";
+        System.out.println(sol.solution(s2));
+
+    }
+}
