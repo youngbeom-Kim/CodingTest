15fe8a53634863a2acf0baa2701b3e69d82c76cd
youngbeom-Kim <abcd450196@gmail.com>
2023-03-14 23:35:00 +0900

    Level 01 > 햄버거 만들기

--- src/level01/Q57.java
+++ src/level01/Q57.java
@@ -7,10 +7,38 @@ public class Q57 {
     public int solution(int[] ingredient) {
         int answer = 0;
 
-        Stack stack = new Stack();
+        /*
+        시간초과 오류 발생
+        String str = "";
 
         for (int i = 0; i < ingredient.length; i++) {
+            str += ingredient[i];
+        }
 
+        while (str.contains("1231")) {
+            str = str.replace("1231", "");
+            answer++;
+        }
+        */
+
+        Stack<Integer> stack = new Stack<>();
+        String str = "";
+
+        for (int i = 0; i < ingredient.length; i++) {
+            stack.push(ingredient[i]);
+
+            if (stack.size() >= 4) {
+                if (stack.get(stack.size() - 1) == 1 &&
+                    stack.get(stack.size() - 2) == 3 &&
+                    stack.get(stack.size() - 3) == 2 &&
+                    stack.get(stack.size() - 4) == 1) {
+                    answer++;
+                    stack.pop();
+                    stack.pop();
+                    stack.pop();
+                    stack.pop();
+                }
+            }
         }
 
         return answer;
@@ -20,10 +48,10 @@ public class Q57 {
 
         Q57 sol = new Q57();
 
-        int[] ingredient = {};
+        int[] ingredient = {2, 1, 1, 2, 3, 1, 2, 3, 1};
         System.out.println(sol.solution(ingredient)); // 2
 
-        int[] ingredient2 = {};
+        int[] ingredient2 = {1, 3, 2, 1, 2, 1, 3, 1, 2};
         System.out.println(sol.solution(ingredient2)); // 0
 
     }
