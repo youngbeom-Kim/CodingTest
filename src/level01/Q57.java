7248611faad9b97059ec0643104f527b91294790
youngbeom-Kim <abcd450196@gmail.com>
2023-03-14 20:24:24 +0900

    Level 01 > 햄버거 만들기

---
+++ src/level01/Q57.java
@@ -0,0 +1,30 @@
+package level01;
+
+import java.util.Stack;
+
+//프로그래머스 > Level 01 > 햄버거 만들기
+public class Q57 {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+
+        Stack stack = new Stack();
+
+        for (int i = 0; i < ingredient.length; i++) {
+
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Q57 sol = new Q57();
+
+        int[] ingredient = {};
+        System.out.println(sol.solution(ingredient)); // 2
+
+        int[] ingredient2 = {};
+        System.out.println(sol.solution(ingredient2)); // 0
+
+    }
+}
