d4ca9443d281c073a05be1347d1b2085f79c0bb9
youngbeom-Kim <abcd450196@gmail.com>
2023-03-16 19:29:47 +0900

    Level 01 > 둘만의 암호

--- src/level01/Q58.java
+++ src/level01/Q58.java
@@ -4,6 +4,27 @@ package level01;
 public class Q58 {
     public String solution(String s, String skip, int index) {
         String answer = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            char sChar = s.charAt(i);
+            for (int j = 0; j < skip.length(); j++) {
+                char skipChar = skip.charAt(j);
+                if (sChar + index - skipChar >= 0) {
+                    sChar = (char) (sChar + 1);
+                }
+                if (sChar + index > 'z') {
+                    sChar = (char) (sChar + index - 26);
+                    if (sChar - skipChar <= 0) {
+                        sChar = (char) (sChar + 1);
+                    }
+                }
+            }
+            sChar = (char) (sChar + index);
+
+            answer += sChar;
+
+        }
+
         return answer;
     }
 
