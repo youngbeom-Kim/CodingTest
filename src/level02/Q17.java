7d3e80990841220a29490fd89b680d218926b292
youngbeom-Kim <abcd450196@gmail.com>
2023-04-01 05:56:19 +0900

    Level 02 > 기능 개발

---
+++ src/level02/Q17.java
@@ -0,0 +1,28 @@
+package level02;
+
+import java.util.Arrays;
+
+//프로그래머스 > Level 02 > 기능 개발
+public class Q17 {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Q17 sol = new Q17();
+
+        int[] progresses = {93, 30, 55};
+        int[] speeds = {1, 30, 5};
+        System.out.println(Arrays.toString(sol.solution(progresses, speeds)));
+
+        int[] progresses2 = {95, 90, 99, 99, 80, 99};
+        int[] speeds2 = {1, 1, 1, 1, 1, 1};
+        System.out.println(Arrays.toString(sol.solution(progresses2, speeds2)));
+
+    }
+}
