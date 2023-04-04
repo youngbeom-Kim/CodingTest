f77eb6d4a63fca824f8716cc6e81891b03a8f98f
youngbeom-Kim <abcd450196@gmail.com>
2023-04-04 17:57:33 +0900

    Level 02 > 기능 개발

--- src/level02/Q17.java
+++ src/level02/Q17.java
@@ -1,11 +1,36 @@
 package level02;
 
-import java.util.Arrays;
+import java.util.*;
 
 //프로그래머스 > Level 02 > 기능 개발
 public class Q17 {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
+        int[] temp = new int[100];
+        int day = 0;
+
+        for (int i = 0; i < progresses.length; i++) {
+            while (progresses[i] + (speeds[i] * day) < 100) {
+                day++;
+            }
+            temp[day]++;
+        }
+
+        int count = 0;
+
+        for (int n : temp) {
+            if (n != 0) {
+                count++;
+            }
+        }
+
+        int[] answer = new int[count];
+
+        count = 0;
+        for (int n : temp) {
+            if (n != 0) {
+                answer[count++]  = n;
+            }
+        }
 
         return answer;
     }
