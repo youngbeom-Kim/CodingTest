9dfa968a52867e568e635905d21858df26252f18
youngbeom-Kim <abcd450196@gmail.com>
2023-04-07 02:30:51 +0900

    Level 02 > n^2 배열 자르기

---
+++ src/level02/Q19.java
@@ -0,0 +1,15 @@
+package level02;
+
+//프로그래머스 > Level 02 > n^2 배열 자르기
+public class Q19 {
+    public int[] solution(int n, long left, long right) {
+        int[] answer = {};
+
+
+
+        return answer;
+    }
+    public static void main(String[] args) {
+
+    }
+}
