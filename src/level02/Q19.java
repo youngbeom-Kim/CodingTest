391ed9db57b7dd36b6d8929af1be387dea54ae0a
youngbeom-Kim <abcd450196@gmail.com>
2023-04-06 22:12:36 +0900

    Level 02 > n^2 배열 자르기

--- src/level02/Q19.java
+++ src/level02/Q19.java
@@ -1,5 +1,7 @@
 package level02;
 
+import java.util.Arrays;
+
 //프로그래머스 > Level 02 > n^2 배열 자르기
 public class Q19 {
     public int[] solution(int n, long left, long right) {
@@ -10,6 +12,18 @@ public class Q19 {
         return answer;
     }
     public static void main(String[] args) {
+
+        Q19 sol = new Q19();
+
+        int n = 3;
+        long left = 2;
+        long right = 5;
+        System.out.println(Arrays.toString(sol.solution(n, left, right)));
+
+        int n2 = 3;
+        long left2 = 2;
+        long right2 = 5;
+        System.out.println(Arrays.toString(sol.solution(n2, left2, right2)));
 
     }
 }
