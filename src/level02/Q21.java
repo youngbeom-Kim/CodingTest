a118c19a261f0e997c63fd47fa82c781f336704a
youngbeom-Kim <abcd450196@gmail.com>
2023-04-16 12:31:48 +0900

    Level 02 > 괄호 회전하기

--- src/level02/Q21.java
+++ src/level02/Q21.java
@@ -2,4 +2,26 @@ package level02;
 
 //프로그래머스 > Level 02 > 괄호 회전하기
 public class Q21 {
+    public int solution(String s) {
+        int answer = -1;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Q21 sol = new Q21();
+
+        String s = "[](){}";
+        System.out.println(sol.solution(s));
+
+        String s2 = "}]()[{";
+        System.out.println(sol.solution(s2));
+
+        String s3 = "[)(]";
+        System.out.println(sol.solution(s3));
+
+        String s4 = "}}}";
+        System.out.println(sol.solution(s4));
+
+    }
 }
