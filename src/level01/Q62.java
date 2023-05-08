48ceb8432dacba8760a268634b5d8ca0bf258f24
youngbeom-Kim <abcd450196@gmail.com>
2023-05-08 19:48:54 +0900

    Level 01 > 공원 산책

--- src/level01/Q62.java
+++ src/level01/Q62.java
@@ -17,6 +17,34 @@ public class Q62 {
             String[] route = routes[i].split(" ");
             String dir = route[0];
             int dis = Integer.valueOf(route[1]);
+            int move = 0;
+            if (dir.equals("E")) {
+                move = 1;
+            } else if (dir.equals("W")) {
+                move = -1;
+            } else if (dir.equals("S")) {
+                move = cnt;
+            } else if (dir.equals("N")) {
+                move = -cnt;
+            }
+
+            //만약 가는 길에 장애물이 있을 경우
+            int check = start;
+
+            for (int j = 0; j < dis; j++) {
+                check += move;
+                if (parks.charAt(check) == 'X') {
+                    check = start;
+                    break;
+                }
+            }
+
+            if (check != (start + (dis * move))) {
+                continue;
+            }
+
+            //거리 이동
+            start += move * dis;
 
             //최대 거리 이상 또는 최소 거리 이하로 이동하면 최대 거리에 머무르기
             if (start > parks.length()) {
@@ -25,19 +53,9 @@ public class Q62 {
                 start = 0;
             }
 
-            //만약 가는 길에 장애물이 있을 시
-
-            //거리 이동
-            if (dir.equals("E")) {
-                start += 1;
-            } else if (dir.equals("W")) {
-                start -= 1;
-            } else if (dir.equals("N")) {
-                start += 3;
-            } else if (dir.equals("S")) {
-                start -= 3;
-            }
         }
+
+        answer = new int[]{start / park.length, start % park.length};
 
         return answer;
     }
@@ -47,11 +65,11 @@ public class Q62 {
 
         String[] park = {"SOO", "OOO", "OOO"};
         String[] routes = {"E 2","S 2","W 1"};
-        System.out.println(Arrays.toString(sol.solution(park, routes)));
+        //System.out.println(Arrays.toString(sol.solution(park, routes)));
 
         String[] park2 = {"SOO", "OXX", "OOO"};
         String[] routes2 = {"E 2","S 2","W 1"};
-        System.out.println(Arrays.toString(sol.solution(park2, routes2)));
+        //System.out.println(Arrays.toString(sol.solution(park2, routes2)));
 
         String[] park3 = {"OSO","OOO","OXO","OOO"};
         String[] routes3 = {"E 2","S 3","W 1"};
