236a3379c2cdbbad996b42e4c6fb2e225a827b30
youngbeom-Kim <abcd450196@gmail.com>
2023-05-20 14:23:25 +0900

    Level 02 > 피로도

---
+++ src/level01/Q63.java
@@ -0,0 +1,18 @@
+package level01;
+
+public class Q63 {
+    public int solution(int k, int[][] dungeons) {
+        int answer = -1;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Q63 sol = new Q63();
+
+        int k = 80;
+        int[][] dungeons = {{80,20},{50,40},{30,10}};
+        System.out.println(sol.solution(k, dungeons));
+
+    }
+}
