ec17ff122ae92a7a750c181b45d984ebdb72f64b
youngbeom-Kim <abcd450196@gmail.com>
2023-05-26 20:15:35 +0900

    Q26 > Level 02 > 주식 가격

---
+++ src/level02/Q26.java
@@ -0,0 +1,23 @@
+package level02;
+
+import java.util.Arrays;
+
+//프로그래머스 > Level 02 > 주식 가격
+public class Q26 {
+    public int[] solution(int[] prices) {
+        int[] answer = {};
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Q26 sol = new Q26();
+
+        int[] prices = {1, 2, 3, 2, 3};
+        System.out.println(Arrays.toString(sol.solution(prices)));
+
+    }
+}
