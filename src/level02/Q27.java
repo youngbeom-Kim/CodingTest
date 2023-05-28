f1ff873d899d5838df1387978c1c83d2ac397e48
youngbeom-Kim <abcd450196@gmail.com>
2023-05-28 17:11:31 +0900

    Level 02 > 땅따먹기

---
+++ src/level02/Q27.java
@@ -0,0 +1,18 @@
+package level02;
+
+//프로그래머스 > Level 02 > 땅따먹기
+public class Q27 {
+    int solution(int[][] land) {
+        int answer = 0;
+
+        return answer;
+    }
+    public static void main(String[] args) {
+
+        Q27 sol = new Q27();
+
+        int[][] land = {{1,2,3,5},{5,6,7,8},{4,3,2,1}};
+        System.out.println(sol.solution(land));
+
+    }
+}
