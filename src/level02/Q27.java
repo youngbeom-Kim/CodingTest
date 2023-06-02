d62de4e8a77d9879b0b22ad8b642031d2ff9f76c
youngbeom-Kim <abcd450196@gmail.com>
2023-06-02 19:37:25 +0900

    Level 02 > 땅따먹기

--- src/level02/Q27.java
+++ src/level02/Q27.java
@@ -5,7 +5,16 @@ public class Q27 {
     int solution2(int[][] land) {
         int answer = 0;
 
+        for (int i = 1; i < land.length; i++) {
+            land[i][0] += Math.max(land[i - 1][1], Math.max(land[i - 1][2], land[i - 1][3]));
+            land[i][1] += Math.max(land[i - 1][0], Math.max(land[i - 1][2], land[i - 1][3]));
+            land[i][2] += Math.max(land[i - 1][0], Math.max(land[i - 1][1], land[i - 1][3]));
+            land[i][3] += Math.max(land[i - 1][0], Math.max(land[i - 1][1], land[i - 1][2]));
+        }
 
+        for (int i = 0; i < 4; i++) {
+            answer = Math.max(answer, land[land.length - 1][i]);
+        }
 
         return answer;
     }
@@ -38,10 +47,10 @@ public class Q27 {
         Q27 sol = new Q27();
 
         int[][] land = {{1,2,3,5},{5,6,7,8},{4,3,2,1}};
-        //System.out.println(sol.solution(land));
+        System.out.println(sol.solution2(land));
 
         int[][] land2 = {{1, 1, 3, 1}, {2, 3, 2, 2}, {1, 4, 1, 1}};
-        System.out.println(sol.solution(land2));
+        System.out.println(sol.solution2(land2));
 
     }
 }
