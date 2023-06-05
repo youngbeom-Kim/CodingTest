2d50879d19b7d0fde6c845476357ac10c82fb76e
youngbeom-Kim <abcd450196@gmail.com>
2023-06-05 14:45:46 +0900

    Level 02 > 땅따먹기

--- src/level02/Q27.java
+++ src/level02/Q27.java
@@ -5,7 +5,6 @@ public class Q27 {
     int solution2(int[][] land) {
         int answer = 0;
 
-
         for (int i = 1; i < land.length; i++) {
             land[i][0] += Math.max(land[i - 1][1], Math.max(land[i - 1][2], land[i - 1][3]));
             land[i][1] += Math.max(land[i - 1][0], Math.max(land[i - 1][2], land[i - 1][3]));
