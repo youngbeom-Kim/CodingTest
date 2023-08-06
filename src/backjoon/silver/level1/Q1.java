4cb1c3a3a31d11ee9001f39e15399acef291e960
youngbeom-Kim <abcd450196@gmail.com>
2023-08-06 22:22:23 +0900

    백준 > 실버5 > 분수 찾기

---
+++ src/backjoon/silver/level1/Q1.java
@@ -0,0 +1,16 @@
+package backjoon.silver.level1;
+
+import java.util.Scanner;
+
+//백준 > 실버5 > 분수 찾기
+public class Q1 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int num = scan.nextInt();
+
+        System.out.println(num);
+
+    }
+}
