0af14efdaa45384fcee3a64ba0cbb34341721477
youngbeom-Kim <abcd450196@gmail.com>
2023-08-27 17:29:49 +0900

    백준 > 실버4 > 괄호

---
+++ src/backjoon/silver/level2/Q02_9012.java
@@ -0,0 +1,20 @@
+package backjoon.silver.level2;
+
+import java.util.Scanner;
+
+//백준 > 실버4 > 괄호 (9012번)
+public class Q02_9012 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int num = in.nextInt();
+
+        String[] arr = new String[num];
+
+        for (int i = 0; i < num; i++) {
+            arr[i] = in.nextLine();
+        }
+
+    }
+}
