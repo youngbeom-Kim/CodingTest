8e002461ae3dae48a515ec14e4fff157cc1df0f4
youngbeom-Kim <abcd450196@gmail.com>
2023-09-29 21:53:11 +0900

    백준 > 실버3 > N과 M(1) (15649번)

--- src/backjoon/silver/level3/Q04_15649.java
+++ src/backjoon/silver/level3/Q04_15649.java
@@ -1,8 +1,19 @@
 package backjoon.silver.level3;
 
+import java.util.Scanner;
+
 //백준 > 실버3 > N과 M(1) (15649번)
 public class Q04_15649 {
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        int M = in.nextInt();
+
+        int[][] arr = new int[N][M];
+
+        StringBuilder sb = new StringBuilder();
 
 
     }
