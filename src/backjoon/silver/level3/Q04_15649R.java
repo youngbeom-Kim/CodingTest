64185617d111509c27ac3a7f330e326c30f1b86a
youngbeom-Kim <abcd450196@gmail.com>
2023-09-30 15:32:03 +0900

    백준 > 실버3 > N과 M(1) (15649번) - 성공 / 백트래킹 R

--- src/backjoon/silver/level3/Q04_15649R.java
+++ src/backjoon/silver/level3/Q04_15649R.java
@@ -3,7 +3,7 @@ package backjoon.silver.level3;
 import java.util.Scanner;
 
 //백준 > 실버3 > N과 M(1) (15649번)
-public class Q04_15649 {
+public class Q04_15649R {
 
     static boolean[] visit;
     static int[] arr;
