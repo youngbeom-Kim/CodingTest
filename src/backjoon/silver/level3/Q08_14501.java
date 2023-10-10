27dda61733677523f5da21e88bf4bdb7b973bce0
youngbeom-Kim <abcd450196@gmail.com>
2023-10-10 15:06:43 +0900

    백준 > 실버3 > 퇴사 (14501번) - 실패

--- src/backjoon/silver/level3/Q08_14501.java
+++ src/backjoon/silver/level3/Q08_14501.java
@@ -4,9 +4,8 @@ import java.util.Scanner;
 
 //백준 > 실버3 > 퇴사 (14501번)
 public class Q08_14501 {
-    static int N, max;
-    static int cnt = 1;
-    static int[] term, pay;
+    static int N, D, max;
+    static int[] term, pay, dp;
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
@@ -15,6 +14,8 @@ public class Q08_14501 {
 
         term = new int[N];
         pay = new int[N];
+        dp = new int[N];
+        D = N;
         max = 0;
 
         for (int i = 0; i < N; i++) {
@@ -22,25 +23,32 @@ public class Q08_14501 {
             pay[i] = in.nextInt();
         }
 
-        recur(1, 0);
+        for (int i = N; i > 0; i--) {
+            dp[i - 1] = (term[i - 1] + i - 1) <= N ? pay[i - 1] : 0;
+        }
+
+        recur(N);
 
         System.out.println(max);
     }
 
-    private static void recur(int date, int paid) {
+    private static void recur(int date) {
 
-        if (cnt == N) {
+        if (D == 0) {
             return;
         }
 
-        if ((term[date - 1] - 1) + date > N) {
-            max = Math.max(max, paid);
-            cnt += 1;
-            recur(cnt, 0);
+        date += term[date - 1];
+
+        if (date <= N) {
+            dp[D - 1] += dp[date - 1];
+            date += term[date - 1];
         }
 
-        paid += pay[date - 1];
+        max = Math.max(max, dp[D - 1]);
 
-        recur(date + term[date - 1], paid);
+        D -= 1;
+
+        recur(D);
     }
 }
