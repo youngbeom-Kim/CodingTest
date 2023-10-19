4901c694268fd1d3199e2d16df860fb9b02a25ec
youngbeom-Kim <abcd450196@gmail.com>
2023-10-19 15:36:23 +0900

    프리코스 1주차 > 7번 문제

--- src/precourse/five/one/problem07.java
+++ src/precourse/five/one/problem07.java
@@ -1,4 +1,19 @@
 package precourse.five.one;
 
 public class problem07 {
+    public static void main(String[] args) {
+
+        String user = "mark";
+        String[][] friends = {{"donut", "andole"}, {"donut", "jun"}, {"donut", "mrko"}, {"shakevan", "andole"}, {"shakevan", "jun"}, {"shakevan", "mrko"}};
+        String[] visitors = {"bedi", "bedi", "donut", "bedi", "shakevan"};
+
+
+
+    }
+
+    private static String[] solution(String user, String[][] friends, String[] visitors) {
+        String[] result = {};
+
+        return result;
+    }
 }
