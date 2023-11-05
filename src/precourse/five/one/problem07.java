2d4b76218201bdd34f28725b5b1f2a0b8737b341
youngbeom-Kim <abcd450196@gmail.com>
2023-11-05 20:59:23 +0900

    프리코스 1주차 > 7번 문제

--- src/precourse/five/one/problem07.java
+++ src/precourse/five/one/problem07.java
@@ -6,8 +6,6 @@ public class problem07 {
         String user = "mark";
         String[][] friends = {{"donut", "andole"}, {"donut", "jun"}, {"donut", "mrko"}, {"shakevan", "andole"}, {"shakevan", "jun"}, {"shakevan", "mrko"}};
         String[] visitors = {"bedi", "bedi", "donut", "bedi", "shakevan"};
-
-
     }
 
     private static String[] solution(String user, String[][] friends, String[] visitors) {
