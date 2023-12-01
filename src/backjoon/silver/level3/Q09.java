50ab5254743714f2e26def4bd435c053f292527a
youngbeom-Kim <abcd450196@gmail.com>
2023-12-01 18:38:51 +0900

    백준 > 실버3 > 패션왕 신해빈 (실패)

---
+++ src/backjoon/silver/level3/Q09.java
@@ -0,0 +1,45 @@
+package backjoon.silver.level3;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+//백준 > 실버3 > 패션왕 신해빈
+public class Q09 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int T = in.nextInt();
+
+        while (T-- > 0) {
+
+            HashMap<String, Integer> clothes = new HashMap<>();
+
+            int N = in.nextInt();
+
+            while(N-- > 0) {
+
+                in.next();
+
+                String type = in.next();
+
+                if (clothes.containsKey(type)) {
+                    clothes.put(type, clothes.get(type) + 1);
+                } else {
+                    clothes.put(type, 1);
+                }
+
+            }
+
+            int result = 1;
+
+            for (int value : clothes.values()) {
+                result *= value;
+            }
+
+            System.out.println(result - 1);
+
+        }
+
+    }
+}
