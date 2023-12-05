c3f27e3b748064a5261b1dada19fc946d49d8fb8
youngbeom-Kim <abcd450196@gmail.com>
2023-12-05 16:07:36 +0900

    백준 > 실버3 > 파도반 수열 (성공)

--- src/backjoon/silver/level3/Q11_9461.java
+++ src/backjoon/silver/level3/Q11_9461.java
@@ -2,7 +2,7 @@ package backjoon.silver.level3;
 
 import java.util.Scanner;
 
-//백준 > 실버3 > 파도반 수
+//백준 > 실버3 > 파도반 수열
 public class Q11_9461 {
     public static void main(String[] args) {
 
@@ -10,22 +10,27 @@ public class Q11_9461 {
 
         int T = in.nextInt();
 
+        StringBuilder sb = new StringBuilder();
+
         while (T-- > 0) {
 
             int N = in.nextInt();
 
-            int[] answer = new int[N + 5];
-            answer[0] = 1;
+            long[] answer = new long[101];
             answer[1] = 1;
             answer[2] = 1;
-            answer[3] = 2;
-            answer[4] = 2;
+            answer[3] = 1;
 
-            for (int i = 5; i < N; i++) {
-                answer[i] = answer[i - 1] + answer[i - 5];
+
+            for (int i = 4; i <= N; i++) {
+                answer[i] = answer[i - 2] + answer[i - 3];
             }
 
-            System.out.println(answer[N - 1]);
+            sb.append(answer[N] + "\n");
         }
+
+        sb.deleteCharAt(sb.length() - 1);
+
+        System.out.println(sb);
     }
 }
