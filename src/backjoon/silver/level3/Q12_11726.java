80f9d89fc1a4567d46e14e72df423fcb860ced98
youngbeom-Kim <abcd450196@gmail.com>
2023-12-06 13:54:29 +0900

    백준 > 실버3 > 2*n 타일링 (성공)

--- src/backjoon/silver/level3/Q12_11726.java
+++ src/backjoon/silver/level3/Q12_11726.java
@@ -10,17 +10,15 @@ public class Q12_11726 {
 
         int N = in.nextInt();
 
-        long[] answer = new long[N + 1];
+        int[] answer = new int[N + 1];
 
-        answer[0] = 0;
+        answer[0] = 1;
         answer[1] = 1;
-        answer[2] = 2;
 
-        for (int i = 3; i <= N; i++) {
-            answer[i] = answer[i - 2] + answer[i - 1];
+        for (int i = 2; i <= N; i++) {
+            answer[i] = (answer[i - 2] + answer[i - 1]) % 10007;
         }
 
-        System.out.println(answer[N] % 10007);
-
+        System.out.println(answer[N]);
     }
 }
