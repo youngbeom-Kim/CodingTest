634e20c613aecd6c4c7559f7639fde41f1aa28ed
youngbeom-Kim <abcd450196@gmail.com>
2023-12-13 22:58:47 +0900

    백준 > 실버5 > 팩토리얼 0의 개수(성공)

--- src/backjoon/silver/level1/Q12_1676.java
+++ src/backjoon/silver/level1/Q12_1676.java
@@ -2,29 +2,19 @@ package backjoon.silver.level1;
 
 import java.util.Scanner;
 
-//백준 > 실버5 > 팩토리 0의 개수
+//백준 > 실버5 > 팩토리얼 0의 개수
 public class Q12_1676 {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
 
-        long num = 1;
-
         int N = in.nextInt();
-
-        for (long i = 1; i <= N; i++) {
-            num *= i;
-        }
-
-        String str = String.valueOf(num);
 
         int cnt = 0;
 
-        for (int i = str.length() - 1; i > 0; i--) {
-            if (str.charAt(i) != '0') {
-                break;
-            }
-            cnt++;
+        while (N > 1) {
+            cnt += N / 5;
+            N = N / 5;
         }
 
         System.out.println(cnt);
