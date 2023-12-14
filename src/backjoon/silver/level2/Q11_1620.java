6dcbdfc3dbe7c0330b336722a3d9c6bb0a8783a3
youngbeom-Kim <abcd450196@gmail.com>
2023-12-14 19:27:57 +0900

    백준 > 실버4 > 나는야 포켓몬 마스터 이다솜(1620번) (성공)

--- src/backjoon/silver/level2/Q11_1620.java
+++ src/backjoon/silver/level2/Q11_1620.java
@@ -3,47 +3,42 @@ package backjoon.silver.level2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map.Entry;
+import java.util.StringTokenizer;
 
 //백준 > 실버4 > 나는야 포켓몬 마스터 이다솜(1620번)
 public class Q11_1620 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String N = br.readLine();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        List<Integer> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        HashMap<Integer, String> type1 = new HashMap<>();
+        HashMap<String, Integer> type2 = new HashMap<>();
 
-        for (int i = 0; i < 2; i++) {
-            list.add(Integer.parseInt(N.split(" ")[i]));
+        for (int i = 1; i <= N; i++) {
+            String monster = br.readLine();
+            type1.put(i, monster);
+            type2.put(monster, i);
         }
 
-        HashMap<String, String> map = new HashMap<>();
-
-        for (int i = 1; i <= list.get(0); i++) {
-            map.put(String.valueOf(i), br.readLine());
-        }
-
-        int cnt = list.get(1);
-
-        while (cnt-- > 0) {
-            String M = br.readLine();
-
-            if (M.charAt(0) > 'A' && M.charAt(0) < 'Z') {
-                for (Entry<String, String> entrySet : map.entrySet()) {
-
-                    if (M.equals(entrySet.getValue())) {
-                        System.out.println(entrySet.getKey());
-                    }
-                }
+        for (int i = 0; i < M; i++) {
+            String Q = br.readLine();
+            if (Q.charAt(0) >= '0' && Q.charAt(0) <= '9') {
+                sb.append(type1.get(Integer.parseInt(Q)));
+                sb.append("\n");
             } else {
-                System.out.println(map.get(M));
+                sb.append(type2.get(Q));
+                sb.append("\n");
             }
         }
 
+        sb.deleteCharAt(sb.length() - 1);
+
+        System.out.println(sb);
     }
 }
