73c645c56ab2a16b361fbd248f38bda6dad8fa13
youngbeom-Kim <abcd450196@gmail.com>
2023-12-29 20:27:25 +0900

    feat: 백준 > 실버2 > 나무 자르기(성공)

--- src/backjoon/silver/level4/Q05_2805.java
+++ src/backjoon/silver/level4/Q05_2805.java
@@ -15,10 +15,37 @@ public class Q05_2805 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < N; i++) {
+        int[] tree = new int[N];
 
+        int min = 0;
+        int max = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            tree[i] = Integer.parseInt(st.nextToken());
+
+            if (max < tree[i]) max = tree[i];
+        }
+
+        while (min < max) {
+
+            int mid = (min + max) / 2;
+            long sum = 0;
+            for (int cut : tree) {
+                if (cut - mid > 0) {
+                    sum += (cut - mid);
+                }
+            }
+
+            if (sum < M) {
+                max = mid;
+            } else {
+                min = mid + 1;
+            }
 
         }
+
+        System.out.println(min - 1);
 
     }
 }
