6e3bff8deb4c319e2b327a0ceee846680f96bcb7
youngbeom-Kim <abcd450196@gmail.com>
2024-01-07 05:52:05 +0900

    feat: 백준 > 실버1 > 케빈 베이컨의 6단계 법칙 (다시 풀기) 플루이드 워셜 알고리즘

--- src/backjoon/silver/level5/Q02_1389.java
+++ src/backjoon/silver/level5/Q02_1389.java
@@ -1,9 +1,62 @@
 package backjoon.silver.level5;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 //백준 > 실버1 > 케빈 베이컨의 6단계 법칙
 public class Q02_1389 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[N + 1][N + 1];
+
+        final int INF = 99999;
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i != j) {
+                    arr[i][j] = INF;
+                }
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr[a][b] = arr[b][a] = 1;
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    arr[i][j] = Math.min(arr[i][j], arr[i][k] + arr[k][j]);
+                }
+            }
+        }
+
+        int min = Integer.MAX_VALUE;
+        int data = 0;
+
+        for (int i = 1; i <= N; i++) {
+            int sum = 0;
+            for (int j = 1; j <= N; j++) {
+                sum += arr[i][j];
+            }
+            if (min > sum) {
+                min = sum;
+                data = i;
+            }
+        }
+
+        System.out.println(data);
 
     }
 }
