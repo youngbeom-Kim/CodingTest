da0b44d10560514e3312b8a5983a59210b26a796
youngbeom-Kim <abcd450196@gmail.com>
2024-01-12 22:39:09 +0900

    feat: 백준 > 실버1 > 미로 탐색(2178번) - 성공 bfs알고리즘

---
+++ src/backjoon/silver/level5/Q06_2667.java
@@ -0,0 +1,14 @@
+package backjoon.silver.level5;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Q06_2667 {
+    public static void main(String[] args) throws IOException {
+
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+
+    }
+}
