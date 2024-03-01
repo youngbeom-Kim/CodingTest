14d54170130a804c92f59d7562a8817ec4265df9
youngbeom-Kim <abcd450196@gmail.com>
2024-03-01 23:25:34 +0900

    feat: 백준 > 실버3 > 바이러스 (성공) dfs

--- src/backjoon/silver/level3/Q02_2606R.java
+++ src/backjoon/silver/level3/Q02_2606R.java
@@ -1,47 +1,51 @@
 package backjoon.silver.level3;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 //백준 > 실버3 > 바이러스
 public class Q02_2606R {
 
-    static boolean[] virus;
-    static int[][] comList;
-    static int com, line;
-    static int count = 0;
-    public static void main(String[] args) {
+    private static boolean[] visited;
+    private static int[][] arr;
+    private static int N;
+    private static int count = 0;
 
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
 
-        com = in.nextInt();
-        line = in.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        virus = new boolean[com + 1];
-        comList = new int[com + 1][com + 1];
+        N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < line; i++) {
-            int a = in.nextInt();
-            int b = in.nextInt();
-            comList[a][b] = comList[b][a] = 1;
+        visited = new boolean[N + 1];
+        arr = new int[N + 1][N + 1];
+
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr[a][b] = arr[b][a] = 1;
         }
 
         System.out.println(dfs(1));
 
     }
 
-    private static int dfs(int i) {
+    private static int dfs(int depth) {
 
-        virus[i] = true;
+        visited[depth] = true;
 
-        for (int j = 1; j <= com; j++) {
-            if (comList[i][j] == 1 && virus[j] == false) {
+        for (int i = 1; i <= N; i++) {
+            if (arr[depth][i] == 1 && !visited[i]) {
                 count++;
-                dfs(j);
+                dfs(i);
             }
         }
 
         return count;
-
     }
 }
 
