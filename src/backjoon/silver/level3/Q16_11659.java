e8add1ef4db2399d00da57c0cd7ef79fe1cb1dec
youngbeom-Kim <abcd450196@gmail.com>
2024-03-03 15:30:26 +0900

    feat: 백준 > 실버3 > 구간 합 구하기 4 (시간 초과)

--- src/backjoon/silver/level3/Q16_11659.java
+++ src/backjoon/silver/level3/Q16_11659.java
@@ -1,9 +1,36 @@
 package backjoon.silver.level3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Q16_11659 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int answer = 0;
+            int min = Integer.parseInt(st.nextToken());
+            int max = Integer.parseInt(st.nextToken());
+            for (int j = min - 1; j < max; j++) {
+                answer += arr[j];
+            }
+            System.out.println(answer);
+        }
     }
 }
