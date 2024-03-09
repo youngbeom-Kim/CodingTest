31fefb75231f74b002f80c05f25f4e2ee82ad548
youngbeom-Kim <abcd450196@gmail.com>
2024-03-09 20:56:45 +0900

    feat: 백준 > 실버4 > 카드2(2164번) - 성공 큐

---
+++ src/backjoon/silver/level2/Q19_2164.java
@@ -0,0 +1,29 @@
+package backjoon.silver.level2;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+//백준 > 실버4 > 카드2(2164번)
+public class Q19_2164 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        while(queue.size() != 1) {
+            queue.poll();
+            if (queue.size() == 1) break;
+            queue.add(queue.poll());
+        }
+
+        System.out.println(queue.peek());
+    }
+}
