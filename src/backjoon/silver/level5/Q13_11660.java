573dd4a69e0bc3a29b8e954840dfefea3d295d86
youngbeom-Kim <abcd450196@gmail.com>
2024-05-03 16:20:19 +0900

    feat: 백준 > 실버1 > 구간 합 구하기5(11660) - 성공 dp로 데이터 적재

--- src/backjoon/silver/level5/Q13_11660.java
+++ src/backjoon/silver/level5/Q13_11660.java
@@ -7,8 +7,6 @@ import java.util.StringTokenizer;
 
 //백준 > 실버1 > 구간 합 구하기 5
 public class Q13_11660 {
-
-    private static int[][] dp;
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,12 +16,19 @@ public class Q13_11660 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        dp = new int[N + 1][N + 1];
+        int[][] arr = new int[N + 1][N + 1];
+        int[][] dp = new int[N + 1][N + 1];
 
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= N; j++) {
-                dp[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + arr[i][j];
             }
         }
 
@@ -33,25 +38,13 @@ public class Q13_11660 {
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            sb.append(addNum(x1, y1, x2, y2)).append("\n");
+            int sum = dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1];
+            sb.append(sum).append("\n");
         }
 
         sb.deleteCharAt(sb.length() - 1);
 
         System.out.println(sb);
 
-    }
-
-    private static int addNum(int x1, int y1, int x2, int y2) {
-
-        int sum = 0;
-
-        for (int i = x1; i <= x2; i++) {
-            for(int j = y1; j <= y2; j++) {
-                sum += dp[i][j];
-            }
-        }
-
-        return sum;
     }
 }
